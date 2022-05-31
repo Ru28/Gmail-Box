2cdbbbc01262aa879e8199c52fedddf8025429cf
Rupesh Virani <rupeshvirani01@gmail.com>
2022-05-31 16:17:17 +0530

    added mail app in installed app in settings.py & migrations

--- gmailbox/settings.py
+++ gmailbox/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'mail.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
